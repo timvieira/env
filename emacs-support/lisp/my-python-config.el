59bcfbbc86528bb3c14e42b1c659d8bc7b7c96b0
timv@herman <timv@herman>
2010-08-30 15:29:37 -0400

    added flymake/pylint show error at point (not working perfectly...)

--- emacs-support/lisp/my-python-config.el
+++ emacs-support/lisp/my-python-config.el
@@ -15,13 +15,14 @@
   (interactive
    (list (let* ((word (thing-at-point 'word))
                 (input (read-string 
-                (format "pydoc entry%s: " 
-                    (if (not word) "" (format " (default %s)" word))))))
-           (if (string= input "") 
-	       (if (not word) (error "No pydoc args given")
-             word)			;sinon word
-	     input))))			;sinon input
-  (shell-command (concat py-python-command " -c \"from pydoc import help;help(\'" w "\')\"") "*PYDOCS*")
+                        (format "pydoc entry%s: " 
+                                (if (not word) "" (format " (default %s)" word))))))
+           (if (string= input "")
+               (if (not word) 
+                   (error "No pydoc args given") 
+                 word)	                ; sinon word
+	     input))))			; sinon input
+  (shell-command (concat py-python-command " -c \"from pydoc import help; help(\'" w "\')\"") "*PYDOCS*")
   (view-buffer-other-window "*PYDOCS*" t 'kill-buffer-and-window))
 
 
@@ -50,6 +51,7 @@
 ;; Configure flymake for python
 (when (load "flymake" t)
   (defun flymake-pylint-init ()
+
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
                        'tim-flymake-create-temp-inplace)
                       )
@@ -63,6 +65,46 @@
 
 ;; Set as a minor mode for python
 (add-hook 'python-mode-hook '(lambda () (flymake-mode)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;flymake-ler(file line type text &optional full-file)
+(defun show-fly-err-at-point ()
+  "If the cursor is sitting on a flymake error, display the
+message in the minibuffer"
+  (interactive)
+  (let ((line-no (line-number-at-pos)))
+    (dolist (elem flymake-err-info)
+      (if (eq (car elem) line-no)
+	  (let ((err (car (second elem))))
+	    (message "%s" (fly-pyflake-determine-message err)))))))
+
+(defun fly-pyflake-determine-message (err)
+  "pyflake is flakey if it has compile problems, this adjusts the
+message to display, so there is one ;)"
+  (cond ((not (or (eq major-mode 'Python) (eq major-mode 'python-mode) t)))
+	((null (flymake-ler-file err))
+	 ;; normal message do your thing
+	 (flymake-ler-text err))
+	(t ;; could not compile err
+	 (format "compile error, problem on line %s" (flymake-ler-line err)))))
+
+(defadvice flymake-goto-next-error (after display-message activate compile)
+  "Display the error in the mini-buffer rather than having to mouse over it"
+  (show-fly-err-at-point))
+
+(defadvice flymake-goto-prev-error (after display-message activate compile)
+  "Display the error in the mini-buffer rather than having to mouse over it"
+  (show-fly-err-at-point))
+
+(defadvice flymake-mode (before post-command-stuff activate compile)
+  "Add functionality to the post command hook so that if the
+cursor is sitting on a flymake error the error information is
+displayed in the minibuffer (rather than having to mouse over
+it)"
+  (set (make-local-variable 'post-command-hook)
+       (cons 'show-fly-err-at-point post-command-hook))) 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
