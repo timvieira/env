8e85fed00fb10ddd157cdd44021e4806ccb43647
timv@herman <timv@herman>
2010-12-14 11:30:46 -0500

    copying hgrc file and using rsync instead of scp

--- bin/create-repo-on-jasper.bash
+++ bin/create-repo-on-jasper.bash
@@ -5,8 +5,13 @@ PWD=`pwd`
 xxx=`basename $PWD`
 echo "send $xxx to jasper"
 
+# need to make sure there is a .hg in directory xxx
+
+# use the standard hgrc file we know and love
+cp `dirname $0`/jasper-hgrc .hg/hgrc
+
 cd ..
-scp -r $xxx jasper.cs.umass.edu:~/my-scratch/
+rsync -r -v $xxx jasper.cs.umass.edu:~/my-scratch/
 
 # ln ../$xxx $xxx
 ssh jasper.cs.umass.edu "cd my-scratch/hg-served-projects && ln -s ../$xxx $xxx && ./kill-server && ./start-server"
