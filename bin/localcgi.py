3a2f012e0f56e562fb0d4911fbd747d98efea512
timv <tim.f.vieira@gmail.com>
2012-06-20 19:02:38 -0400

    ()

--- bin/localcgi.py
+++ bin/localcgi.py
@@ -6,7 +6,7 @@ import BaseHTTPServer
 class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
     cgi_directories = ["/cgi-bin"]
 
-port = 9999
+port = 80
 httpd = BaseHTTPServer.HTTPServer(("", port), Handler)
 print "serving at port", port
 httpd.serve_forever()
