770155eb54048fec6e563855d5f925ba75d7093e
timv <tim.f.vieira@gmail.com>
2013-12-07 13:45:53 -0800

    fixes to flymake pylint runner.

--- emacs/my-python.el
+++ emacs/my-python.el
@@ -1,30 +1,25 @@
-(add-hook 'python-mode-hook
-          (lambda ()
-            (eldoc-mode 1)
-            (setq outline-regexp "def\\|class ")))
-
-(load-library "pylint")
-(load-library "site-lisp/flymake-cursor")
+;; (load-library "pylint")
+;; (load-library "site-lisp/flymake-cursor")
 
 (defcustom py-python-command "python"
   "*Shell command used to start Python interpreter."
   :type 'string
   :group 'python)
 
-(defun my-python-documentation (w)
-  "Launch PyDOC on the Word at Point"
-  (interactive
-   (list (let* ((word (thing-at-point 'word))
-                (input (read-string
-                        (format "pydoc entry%s: "
-                                (if (not word) "" (format " (default %s)" word))))))
-           (if (string= input "")
-               (if (not word)
-                   (error "No pydoc args given")
-                 word)                  ; sinon word
-             input))))			; sinon input
-  (shell-command (concat py-python-command " -c \"from pydoc import help; help(\'" w "\')\"") "*PYDOCS*")
-  (view-buffer-other-window "*PYDOCS*" t 'kill-buffer-and-window))
+;; new ;; (defun my-python-documentation (w)
+;; new ;;   "Launch PyDOC on the Word at Point"
+;; new ;;   (interactive
+;; new ;;    (list (let* ((word (thing-at-point 'word))
+;; new ;;                 (input (read-string
+;; new ;;                         (format "pydoc entry%s: "
+;; new ;;                                 (if (not word) "" (format " (default %s)" word))))))
+;; new ;;            (if (string= input "")
+;; new ;;                (if (not word)
+;; new ;;                    (error "No pydoc args given")
+;; new ;;                  word)                  ; sinon word
+;; new ;;              input))))			; sinon input
+;; new ;;   (shell-command (concat py-python-command " -c \"from pydoc import help; help(\'" w "\')\"") "*PYDOCS*")
+;; new ;;   (view-buffer-other-window "*PYDOCS*" t 'kill-buffer-and-window))
 
 ;;;; general init-cleanup and helper routines
 (defun tim-flymake-create-temp-inplace (file-name prefix)
@@ -39,7 +34,6 @@
     (flymake-log 3 "create-temp-inplace: file=%s temp=%s" file-name temp-name)
     temp-name))
 
-
 ;; Configure flymake for python
 (when (load "flymake" t)
   (defun flymake-pylint-init ()
@@ -53,12 +47,46 @@
                '("\\.py\\'" flymake-pylint-init)))
 
 
+;; Configure flymake for Python
+;???; (setq pylint "epylint")
+;???; (when (load "flymake" t)
+;???;   (defun flymake-pylint-init ()
+;???;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;???;                        'flymake-create-temp-inplace))
+;???;            (local-file (file-relative-name
+;???;                         temp-file
+;???;                         (file-name-directory buffer-file-name))))
+;???;       (list (expand-file-name pylint "") (list local-file))))
+;???;   (add-to-list 'flymake-allowed-file-name-masks
+;???;                '("\\.py\\'" flymake-pylint-init)))
+
+(defun show-fly-err-at-point ()
+  "If the cursor is sitting on a flymake error, display the message in the minibuffer"
+  (require 'cl)
+  (interactive)
+  (let ((line-no (line-number-at-pos)))
+    (dolist (elem flymake-err-info)
+      (if (eq (car elem) line-no)
+      (let ((err (car (second elem))))
+        (message "%s" (flymake-ler-text err)))))))
+
+;; To avoid having to mouse hover for the error message, these functions make
+;; flymake error messages appear in the minibuffer
+(add-hook 'post-command-hook 'show-fly-err-at-point)
+
+
 (defun my-python-hook ()
   (local-set-key [f7] 'flymake-mode)
 
-  ; type "from debug import ip; ip()"
+  (flymake-mode)
+
+  ; type "from arsenal.debug import ip; ip()"
   (fset 'python-ip [?f ?r ?o ?m ?  ?a ?r ?s ?e ?n ?a ?l ?. ?d ?e ?b ?u ?g ?  ?i ?m ?p ?o ?r ?t ?  ?i ?p ?\; ?  ?i ?p ?\( ?\)])
   (local-set-key (kbd "\C-cb") 'python-ip)
+
+  ;; Keymaps to navigate thru pylint errors
+  (define-key python-mode-map "\C-cn" 'flymake-goto-next-error)
+  (define-key python-mode-map "\C-cp" 'flymake-goto-prev-error)
 
   ;; highlight special comments
   (font-lock-add-keywords
@@ -66,7 +94,7 @@
    '(("\\<\\(FIX\\|TODO\\|XXX\\|FIXME\\|HACK\\|REFACTOR\\):" 1 '(:foreground "orange") t)))
 )
 
-(add-hook 'python-mode-hook 'my-python-hook)
 
-;; Set as a minor mode for python
-(add-hook 'python-mode-hook '(lambda () (flymake-mode)))
+(add-hook 'python-mode-hook 'my-python-hook)
+;(add-hook 'python-mode-hook '(lambda () (flymake-mode)))
+;(add-hook 'python-mode-hook '(lambda () (eldoc-mode 1) (setq outline-regexp "def\\|class ")))
