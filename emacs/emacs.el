cdb1681577738e82b0cab99ed805ef15f7a6e6c7
timv <tim.f.vieira@gmail.com>
2014-05-07 14:31:10 -0400

    ()

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1,4 +1,4 @@
-;; -*- coding: utf-8 -*-
+33;; -*- coding: utf-8 -*-
 ;;
 ;; Notes
 ;; =====
@@ -570,3 +570,38 @@
 ; '(outline-1 ((t (:inherit font-lock-function-name-face :foreground "purple"))))
 ; '(rst-level-2-face ((t (:foreground "Purple2"))) t)
 )
+
+
+
+;; <INDENTING TEXT>
+;;   Code copied from:  http://www.emacswiki.org/emacs/IndentingText
+
+;; Shift the selected region right if distance is postive, left if
+;; negative
+
+; (defun shift-region (distance)
+;   (let ((mark (mark)))
+;     (save-excursion
+;       (indent-rigidly (region-beginning) (region-end) distance)
+;       (push-mark mark t t)
+;       ;; Tell the command loop not to deactivate the mark
+;       ;; for transient mark mode
+;       (setq deactivate-mark nil))))
+; 
+; (defun shift-right ()
+;   (interactive)
+;   (shift-region 1))
+; 
+; (defun shift-left ()
+;   (interactive)
+;   (shift-region -1))
+; 
+; ;; Bind (shift-right) and (shift-left) function to your favorite keys. I use
+; ;; the following so that Ctrl-Shift-Right Arrow moves selected text one 
+; ;; column to the right, Ctrl-Shift-Left Arrow moves selected text one
+; ;; column to the left:
+; 
+; (global-set-key [C-S-right] 'shift-right)
+; (global-set-key [C-S-left] 'shift-left)
+
+;; </INDENTING TEXT>
