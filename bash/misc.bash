7dc22e1746004cc76e50de2a7cfff389b6ba33a6
timv <tim.f.vieira@gmail.com>
2016-03-07 21:16:43 -0500

    ()

--- bash/misc.bash
+++ bash/misc.bash
@@ -12,27 +12,6 @@ function graphviz {
 
 alias gnome-do-restart='(pkill9 gnome-do && shutup-and-disown gnome-do) >& /dev/null'
 
-function extract {
-  if [ -f $1 ] ; then
-    case $1 in
-      *.tar.bz2)   tar xvjf $1   ;;
-      *.tar.gz)    tar xvzf $1   ;;
-      *.bz2)       tar xjfv $1   ;;
-      *.rar)       unrar x $1    ;;
-      *.gz)        gunzip $1     ;;
-      *.tar)       tar xvf $1    ;;
-      *.tbz2)      tar xvjf $1   ;;
-      *.tgz)       tar xvzf $1   ;;
-      *.zip)       unzip $1      ;;
-      *.Z)         uncompress $1 ;;
-      *.7z)        7z x $1       ;;
-      *)           echo "'$1' cannot be extracted via >extract<" ;;
-    esac
-  else
-    echo "'$1' is not a valid file"
-  fi
-}
-
 function o {
     # gnome-open; xdg-open    # unity equivalent of gnome-open
     xdg-open "$@" 2>/dev/null >/dev/null
