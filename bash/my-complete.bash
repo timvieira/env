20160e747362b4e446e436fbdf9588114b04d818
timv <tim.f.vieira@gmail.com>
2016-11-19 15:48:43 -0500

    cleanup projects list and `update` method.

--- bash/my-complete.bash
+++ bash/my-complete.bash
@@ -67,7 +67,7 @@ _complete_vpy ()
 
 
 
-# TODO: clever things to keep things up-to-date?
+# TODO: clever things to keep these files up-to-date?
 COMP_ENV=~/.completions/env
 COMP_NOTES=~/projects/notes/.index/files
 COMP_PROJECTS=~/.completions/projects
@@ -86,62 +86,71 @@ ensuredirs \
     $COMP_NOTES \
     $COMP_PROJECTS
 
+#function find-repos {
+#   find $1 -name '.hg' -type d | sed 's/\\/\.hg$//g'
+#   find $1 -name '.git' -type d | sed 's/\\/\.git$//g'
+#}
+
+function find-repos {
+    locate '*/.hg' | sed 's/\/\.hg$//g'
+    locate '*/.git' | sed 's/\/\.git$//g'
+}
+
 function update {
 
     yellow "Updating completions"
 
     # notes files
     notes.py --files
-    notes.py --update
+#    notes.py --update
 
     # environment files
     find $ENV |ignore-filter |grep -v README |grep -v 'site-lisp' > $COMP_ENV
     ls -x $ENV/emacs/*.el >> $COMP_ENV
 
     # project directories
-    projname=$( echo \
-        $PROJECTS/*/working \
-        $PROJECTS/*/*/working \
-        $PROJECTS/* \
-        $PROJECTS/blab/* \
-        $PROJECTS/self/* \
-        |sed 's/ /\n/g')
+#    projname=$( echo \
+#        $PROJECTS/*/working \
+#        $PROJECTS/*/*/working \
+#        $PROJECTS/* \
+#        $PROJECTS/blab/* \
+#        $PROJECTS/self/* \
+#        |sed 's/ /\n/g')
+#    echo $projname
 
     # courses
     courses=`find $PROJECTS/shelf/courses -type d`
 
     # version controlled project roots -- be sure to strip off hg directories or
     # else they'll get filtered out
-    hg_roots=`find $PROJECTS -name '.hg' -type d | grep -v incoming | sed 's/\\/\.hg$//g'`
-    git_roots=`find $PROJECTS -name '.git' -type d | grep -v incoming | sed 's/\\/\.git$//g'`
+    #hg_roots=`find $PROJECTS -name '.hg' -type d | grep -v incoming | sed 's/\\/\.hg$//g'`
+    #git_roots=`find $PROJECTS -name '.git' -type d | grep -v incoming | sed 's/\\/\.git$//g'`
+    #vcroots="$hg_roots $git_roots"
 
-    vcroots="$hg_roots $git_roots"
+    vcroots=`find-repos`
+#    vcroots=`echo "$vcroots" |sed 's/ /\n/g'`
 
-    #locate "*/.hg"
-    #locate "*/.git"
-
-    # TODO: why did I have this filter "grep -v '/projects/notes/'" above?
-
-    # sort vc roots so that prefixes come first
-    vcroots=`echo "$vcroots" |sed 's/ /\n/g'| bymtime -t -`
-
-    # everything else
-    #everythingelse=`find $PROJECTS -type d`
-
-    matches="$projname
-$courses
+    #matches="$projname
+    matches="$courses
 $vcroots
-$everythingelse"
+"
 
     echo "/home/timv/Dropbox/todo" > $COMP_PROJECTS
     echo "$matches" |ignore-filter \
+        |grep -v pelican-plugins   \
+        |grep -v third-party       \
+        |grep -v incoming          \
         |grep -v bdslss/reviews    \
         |grep -v '/data/'          \
         |grep -v hw7-xfst/xfsm_api \
         |grep -v '~$'              \
-        >> $COMP_PROJECTS
+        |bymtime -t >> $COMP_PROJECTS
 }
 
+
+#function unique-lines {
+#    linepy 'z=set()' 'z.add(line)' 'print "\n".join(sorted(z))'
+#}
 
 # TODO: create a version of hist complete which uses dir-history.
 
