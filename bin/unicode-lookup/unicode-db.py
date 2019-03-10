9bcd701ceb9c82529286a703a7aa52b369327eb9
timv <tim.f.vieira@gmail.com>
2019-03-10 10:19:06 -0700

    py3

--- bin/unicode-lookup/unicode-db.py
+++ bin/unicode-lookup/unicode-db.py
@@ -1,10 +1,11 @@
 """
 Generate a lookup table of unicode characters, their codes, and their names.
 """
+from __future__ import print_function
 import unicodedata
 for i in range(2**16):
     x = eval('u"\\u%04x"' % i)
     try:
-        print x.encode('utf-8'), '|', "%04x" % ord(x), '|', unicodedata.name(x, '-')
-    except IOError:
+        print(x, '|', "%04x" % ord(x), '|', unicodedata.name(x, '-'), sep=' ')
+    except (IOError, UnicodeEncodeError):
         continue
