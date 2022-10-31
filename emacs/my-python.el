6395094bb96d7a02c89312f5c41900bf5a45b56a
timv <tim.f.vieira@gmail.com>
2022-10-31 14:31:05 -0400

    ()

--- emacs/my-python.el
+++ emacs/my-python.el
@@ -84,7 +84,7 @@
 
 (defun show-fly-err-at-point ()
   "If the cursor is sitting on a flymake error, display the message in the minibuffer"
-  (require 'cl)
+  ;(require 'cl)
   (interactive)
   (let ((line-no (line-number-at-pos)))
     (dolist (elem flymake-err-info)
